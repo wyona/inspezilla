999cb9821e892fab0114b8b1e91770900eef30ce
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2005-01-06 00:15:52 +0000

    ignore props added

--- src/inspezilla/content/introspection.js
+++ src/inspezilla/content/introspection.js
@@ -83,6 +83,11 @@ function getIntrospectionURL(pageURL) {
   pageDocument.async = false;
   pageDocument.load(pageURL);
 
+  //logError('getIntrospectionURL', 'LEVI!');
+  //var pageDocument = document.open();
+  //var pageDocument = Document.open(pageURL);
+  //logError('getIntrospectionURL', 'VANYA!');
+
   // Check if any link nodes exist
   var linkNodes = pageDocument.getElementsByTagName('link');
   if (linkNodes.length == 0) {
